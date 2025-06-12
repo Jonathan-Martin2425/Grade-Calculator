83f431fe21ba4516f58731782758909d6de066ce
Jonathan-Martin2425 <jonathan.martin2425@gmail.com>
2025-06-11 21:17:24 -0700

    connect frontend build to backend

---
+++ shared/ValidRoutes.ts
@@ -0,0 +1,9 @@
+export const ValidRoutes = {
+    LOGIN: "/",
+    REGISTER: "/register",
+    HOME: "/homepage",
+    HOME_SCHOOL: "/homepage/:school",
+    QUARTER: "/quarter",
+    QUARTER_SPECIFIED: "/quarter/:quarter",
+    CLASS: "/quarter/:quarter/class/:classItem",
+}
